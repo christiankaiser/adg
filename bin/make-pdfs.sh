aa5618403a21b9b3685393616a8fa448ab08ba17
Christian Kaiser <ck@naxio.ch>
2019-09-18 15:06:56 +0200

    Cleaned up unused files

--- bin/make-pdfs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for f in `find -L . -type f -name "*.md"`; do
-    base="${f%.*}"
-    pandoc -f markdown+tex_math_dollars -t latex --mathml --latex-engine=xelatex -i $f -o $base.pdf
-done
